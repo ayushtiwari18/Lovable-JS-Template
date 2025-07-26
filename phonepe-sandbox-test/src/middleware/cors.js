47b31c137bc3434bff1adc10be5de530f6674000
Archi Jain <jainarchi023@gmail.com>
2025-07-26 19:32:07 +0530

    adjustment2

--- phonepe-sandbox-test/src/middleware/cors.js
+++ phonepe-sandbox-test/src/middleware/cors.js
@@ -172,7 +172,7 @@ class CorsMiddleware {
       `http://localhost:3000`,
       `http://localhost:5173`,
       `http://localhost:3001`,
-      `https://shrifal-handicrafts.onrender.com`,
+      `https://shrifal-handicrafts.netlify.app/`,
     ];
 
     if (process.env.ALLOWED_ORIGINS) {
